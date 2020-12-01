f6d0cae6d6957c6c2367cb69b5e2eb7b6bed976e
Micharzu <micharzuc@gmail.com>
2020-12-01 14:10:15 +0100

    nav draft

---
+++ src/Navigation/Nav.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./nav-styles/Nav.css";
+
+function Nav() {
+  const navTo = (section) => {
+    console.log(section);
+  };
+  return (
+    <div className="navbar">
+      <div className="nav-links">
+        <div className="nav-link" onClick={navTo("home")}>
+          <p>Główna</p>
+        </div>
+        <div className="nav-link" onClick={navTo("news")}>
+          <p>Aktualności</p>
+        </div>
+        <div className="nav-link" onClick={navTo("band")}>
+          <p>Zespół</p>
+        </div>
+        <div className="nav-link" onClick={navTo("galery")}>
+          <p>Galeria</p>
+        </div>
+        <div className="nav-link" onClick={navTo("multimedia")}>
+          <p>Multimedia</p>
+        </div>
+        <div className="nav-link" onClick={navTo("sheets")}>
+          <p>Nuty</p>
+        </div>
+        <div className="nav-link" onClick={navTo("contact")}>
+          <p>Kontakt</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
