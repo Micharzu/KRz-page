5231f11dfc483411b1a0dbe53a1dc40d8473c6a0
Micharzu <micharzuc@gmail.com>
2021-01-22 01:26:30 +0100

    add respo to sheets

--- src/components/Sheets/Sheet.js
+++ src/components/Sheets/Sheet.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./sheets-styles/Sheet.css";
 import PDFViewer from "pdf-viewer-reactjs";
+import { useMediaQuery } from "react-responsive";
 
 function Sheets({
   activeSongTitle,
@@ -10,8 +11,10 @@ function Sheets({
   activeSongSheets,
   activeSongRecording,
 }) {
-  const [numPages, setNumPages] = useState();
-  useEffect(() => {});
+  const isBigScreen = useMediaQuery({ query: "(min-width: 950px)" });
+  const isMobile = useMediaQuery({
+    query: "(max-width: 600px)",
+  });
 
   return (
     <div className="song">
@@ -19,29 +22,82 @@ function Sheets({
       <p className="song-desc">{activeSongDesc}</p>
 
       {activeSongAvailableSheets ? (
-        <div className="active-song-sheets">
-          {/* {activeSongSheets} */}
-          <PDFViewer
-            document={{ url: activeSongSheets }}
-            scale={0.8}
-            showThumbnail={{ scale: 0.2 }}
-            scaleStep={0.1}
-            maxScale={1.1}
-            minScale={0.8}
-            hideRotation={true}
-            navigation={{
-              css: {
-                navbarWrapper: "navbar-wrapper",
-                zoomOutBtn: "zoom-btns btn mat-icon",
-                zoomInBtn: "zoom-btns btn mat-icon",
-                resetZoomBtn: "no-disp",
-                previousPageBtn: "page-nav-btns btn mat-icon",
-                nextPageBtn: "page-nav-btns btn mat-icon",
-                pageIndicator: "page-indicator",
-              },
-            }}
-          />
-        </div>
+        isBigScreen ? (
+          <div className="active-song-sheets">
+            {/* big screen */}
+            BIG
+            <PDFViewer
+              document={{ url: activeSongSheets }}
+              scale={0.9}
+              showThumbnail={{ scale: 0.2 }}
+              scaleStep={0.1}
+              maxScale={1.1}
+              minScale={0.9}
+              hideRotation={true}
+              navigation={{
+                css: {
+                  navbarWrapper: "navbar-wrapper",
+                  zoomOutBtn: "zoom-btns btn mat-icon",
+                  zoomInBtn: "zoom-btns btn mat-icon",
+                  resetZoomBtn: "zoom-btns btn mat-icon",
+                  previousPageBtn: "page-nav-btns btn mat-icon",
+                  nextPageBtn: "page-nav-btns btn mat-icon",
+                  pageIndicator: "page-indicator",
+                },
+              }}
+            />
+          </div>
+        ) : isMobile ? (
+          <div className="active-song-sheets">
+            {/* mobile */}
+            mobile
+            <PDFViewer
+              document={{ url: activeSongSheets }}
+              scale={0.5}
+              showThumbnail={{ scale: 0.2 }}
+              scaleStep={0.1}
+              maxScale={1.1}
+              minScale={0.5}
+              hideRotation={true}
+              navigation={{
+                css: {
+                  navbarWrapper: "navbar-wrapper",
+                  zoomOutBtn: "zoom-btns btn mat-icon",
+                  zoomInBtn: "zoom-btns btn mat-icon",
+                  resetZoomBtn: "zoom-btns btn mat-icon",
+                  previousPageBtn: "page-nav-btns btn mat-icon",
+                  nextPageBtn: "page-nav-btns btn mat-icon",
+                  pageIndicator: "page-indicator",
+                },
+              }}
+            />
+          </div>
+        ) : (
+          <div className="active-song-sheets">
+            {/* small screen */}
+            SMALL
+            <PDFViewer
+              document={{ url: activeSongSheets }}
+              scale={0.7}
+              showThumbnail={{ scale: 0.2 }}
+              scaleStep={0.1}
+              maxScale={1.1}
+              minScale={0.7}
+              hideRotation={true}
+              navigation={{
+                css: {
+                  navbarWrapper: "navbar-wrapper",
+                  zoomOutBtn: "zoom-btns btn mat-icon",
+                  zoomInBtn: "zoom-btns btn mat-icon",
+                  resetZoomBtn: "zoom-btns btn mat-icon",
+                  previousPageBtn: "page-nav-btns btn mat-icon",
+                  nextPageBtn: "page-nav-btns btn mat-icon",
+                  pageIndicator: "page-indicator",
+                },
+              }}
+            />
+          </div>
+        )
       ) : (
         <div className="active-song-no-sheets not-found-label">
           Nie znaleziono nut
--- src/components/Sheets/SheetList.js
+++ src/components/Sheets/SheetList.js
@@ -1,7 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import SheetSample from "F:/react exc/rsz/rsz-strona/src/assets/sheets/test_pdf.pdf";
 import "./sheets-styles/SheetList.css";
 import Sheet from "./Sheet";
+import { BsMusicNoteList as SongListIcon } from "react-icons/bs";
+import { IconContext } from "react-icons";
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from "body-scroll-lock";
+
 function SheetList() {
   const songList = [
     {
@@ -31,34 +39,182 @@ function SheetList() {
       sheets: null,
       recording: null,
     },
+    {
+      id: 3,
+      title: "eeeee",
+      desc: "Yuck",
+      availableSheets: false,
+      availableRecording: false,
+      sheets: null,
+      recording: null,
+    },
+    {
+      id: 4,
+      title: "eeeee",
+      desc: "Yuck",
+      availableSheets: false,
+      availableRecording: false,
+      sheets: null,
+      recording: null,
+    },
+    {
+      id: 5,
+      title: "eeeee",
+      desc: "Yuck",
+      availableSheets: false,
+      availableRecording: false,
+      sheets: null,
+      recording: null,
+    },
+    {
+      id: 6,
+      title: "eeeee",
+      desc: "Yuck",
+      availableSheets: false,
+      availableRecording: false,
+      sheets: null,
+      recording: null,
+    },
+    {
+      id: 7,
+      title: "eeeee",
+      desc: "Yuck",
+      availableSheets: false,
+      availableRecording: false,
+      sheets: null,
+      recording: null,
+    },
+    {
+      id: 8,
+      title: "eeeee",
+      desc: "Yuck",
+      availableSheets: false,
+      availableRecording: false,
+      sheets: null,
+      recording: null,
+    },
+    {
+      id: 9,
+      title: "eeeee",
+      desc: "Yuck",
+      availableSheets: false,
+      availableRecording: false,
+      sheets: null,
+      recording: null,
+    },
+    {
+      id: 10,
+      title: "eeeee",
+      desc: "Yuck",
+      availableSheets: false,
+      availableRecording: false,
+      sheets: null,
+      recording: null,
+    },
+    {
+      id: 11,
+      title: "eeeee",
+      desc: "Yuck",
+      availableSheets: false,
+      availableRecording: false,
+      sheets: null,
+      recording: null,
+    },
+    {
+      id: 12,
+      title: "eeeee",
+      desc: "Yuck",
+      availableSheets: false,
+      availableRecording: false,
+      sheets: null,
+      recording: null,
+    },
   ];
+
   const [activeSong, setActiveSong] = useState();
+  const [listIsOpened, setListIsOpened] = useState(false);
+  const [baseContentHeight, setBaseContentHeight] = useState();
+  const [newContent, setNewContent] = useState();
+  const listRef = useRef();
+  const buttonRef = useRef();
+
+  useEffect(() => {
+    setBaseContentHeight(document.querySelector(".content").offsetHeight);
+    setNewContent(document.querySelector(".content"));
+  }, []);
+
+  useEffect(() => {
+    let offset = Math.ceil((baseContentHeight * 3) / 100) - 0.5;
+    listRef.current.classList.toggle("opened");
+    buttonRef.current.classList.toggle("opened");
+
+    if (baseContentHeight && window.pageYOffset > offset) {
+      window.scrollTo({ top: offset, behavior: "smooth" });
+    }
+    if (listIsOpened) {
+      disableBodyScroll(listRef.current);
+    } else {
+      clearAllBodyScrollLocks();
+    }
+    setListHeight();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [listIsOpened]);
+
+  useEffect(() => {
+    setListHeight();
+    listRef.current.classList.remove("opened");
+    buttonRef.current.classList.remove("opened");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [activeSong]);
+
+  const setListHeight = () => {
+    if (newContent && baseContentHeight) {
+      if (newContent.offsetHeight > baseContentHeight) {
+        listRef.current.style.maxHeight = "89.5%";
+      } else {
+        listRef.current.style.maxHeight = "81.5%";
+      }
+    }
+  };
 
   return (
-    <div className="sheets">
-      <div className="song-list">
-        {songList &&
-          songList.map((song) => (
-            <div
-              key={song.id}
-              className="song-list-item"
-              onClick={() => setActiveSong(song)}
-            >
-              {song.title}
-            </div>
-          ))}
-      </div>
-      <div className="active-song">
-        {activeSong && (
-          <Sheet
-            activeSongTitle={activeSong.title}
-            activeSongDesc={activeSong.desc}
-            activeSongAvailableSheets={activeSong.availableSheets}
-            activeSongAvailableRecording={activeSong.availableRecording}
-            activeSongSheets={activeSong.sheets}
-            activeSongRecording={activeSong.recording}
+    <div className="sheet-list">
+      <div className="song-list-menu" ref={buttonRef}>
+        <IconContext.Provider
+          value={{ size: "2em", color: "rgb(208, 223, 255)" }}
+        >
+          <SongListIcon
+            className="song-list-icon"
+            onClick={() => setListIsOpened(!listIsOpened)}
           />
-        )}
+        </IconContext.Provider>
+      </div>
+      <div className="sheets">
+        <div className="song-list" ref={listRef}>
+          {songList &&
+            songList.map((song) => (
+              <div
+                key={song.id}
+                className="song-list-item"
+                onClick={() => setActiveSong(song)}
+              >
+                {song.title}
+              </div>
+            ))}
+        </div>
+        <div className="active-song">
+          {activeSong && (
+            <Sheet
+              activeSongTitle={activeSong.title}
+              activeSongDesc={activeSong.desc}
+              activeSongAvailableSheets={activeSong.availableSheets}
+              activeSongAvailableRecording={activeSong.availableRecording}
+              activeSongSheets={activeSong.sheets}
+              activeSongRecording={activeSong.recording}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
